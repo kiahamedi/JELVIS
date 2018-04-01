fa4218c1ebea7c965fc6e7296b4d2ff4fa30f9e9
Kia Hamedi <kia.arta9793@gmail.com>
2018-04-01 20:53:30 +0430

    update

--- script.py
+++ script.py
@@ -6,12 +6,16 @@ import time, sys
 import pyttsx3
 import warnings
 import threading
+import pocketsphinx
 from os import system
+from PyQt4 import QtGui
+import speech_recognition as sr
 from PyQt4.QtCore import QSize
 from PyQt4.QtGui import QApplication, QLabel, QMovie, QPainter, QFontMetrics 
+import urllib3
 
 
-mode = "text"
+mode = "voice"
 if len(sys.argv) > 1:
     if sys.argv[1] == "--voice" or sys.argv[1] == "voice":
         try:
@@ -36,7 +40,6 @@ class QTextMovieLabel(QLabel):
         self._movieWidth = s.width()
         self._movieHeight = s.height()
 
- 
 def main():
     import sys
     app = QApplication(sys.argv)
@@ -45,6 +48,12 @@ def main():
     l.show()
     app.exec_()
 
+def internet_on():
+    try:
+        urllib2.urlopen('http://216.58.192.142', timeout=1)
+        return True
+    except urllib2.URLError as err: 
+        return False
 
 def offline_speak(jarvis_speech):
     engine = pyttsx3.init()
@@ -52,18 +61,29 @@ def offline_speak(jarvis_speech):
     engine.runAndWait()
 
 def listen():
+    #try:
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Talk to JELVIS: ")
         audio = r.listen(source)
     try:
-        print r.recognize_google(audio)
-        return r.recognize_google(audio)
+        #print (r.recognize_google(audio))
+        #return  r.recognize_google(audio)
+        if internet_on() == True:
+            print r.recognize_google(audio)
+            return  r.recognize_google(audio)
+        else:
+            print r.recognize_sphinx(audio)
+            return  r.recognize_sphinx(audio)
+
     except sr.UnknownValueError:
         #offline_speak("I couldn't understand what you said! Would you like to repeat?")
         return(listen())
     except sr.RequestError as e:
-        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        print("Could not request results from sphinx service; {0}".format(e))
+    #except sr.UnknownValueError:
+    #    return(listen())
+
 
 kernel = aiml.Kernel()
 
@@ -76,17 +96,20 @@ else:
 # kernel now ready for use
 
 def orgin():
-    while True:
-        if mode == "voice":
-            response = listen()
-        else:
-            response = raw_input("Talk to JELVIS : ")
-        if response.lower().replace(" ","") in terminate:
-            break
-        jarvis_speech = kernel.respond(response)
-        print "JELVIS: " + jarvis_speech
-        offline_speak(jarvis_speech)
-
+    try:
+        while True:
+            if mode == "voice":
+                response = listen()
+            else:
+                response = raw_input("Talk to JELVIS : ")
+            if response.lower().replace(" ","") in terminate:
+                #break
+                response = listen()    
+            jarvis_speech = kernel.respond(response)
+            print ("JELVIS: " + jarvis_speech)
+            offline_speak(jarvis_speech)
+    except:
+       return(orgin())
 
 
 t = threading.Thread(name='orgin', target=orgin)
@@ -94,9 +117,6 @@ w = threading.Thread(name='main', target=main)
 
 w.start()
 t.start()
-    
-
-
 
 
 
