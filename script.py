f6f223bf7fdadc818f439a5f580d0a892a5ad6e9
Kia Hamedi <kia.arta9793@gmail.com>
2018-07-28 02:17:39 +0430

    new update jelvis

--- script.py
+++ script.py
@@ -6,12 +6,11 @@ import warnings
 import threading
 import pocketsphinx
 from os import system
-from PyQt4 import QtGui
+from PyQt4 import QtCore,QtGui
 import speech_recognition as sr
 from PyQt4.QtCore import QSize
 from PyQt4.QtGui import QApplication, QLabel, QMovie, QPainter, QFontMetrics 
 import urllib3
-
 
 mode = "voice"
 if len(sys.argv) > 1:
@@ -21,16 +20,19 @@ if len(sys.argv) > 1:
             mode = "voice"
         except ImportError:
             print("\nInstall SpeechRecognition to use this feature.\nStarting text mode\n")
-
 terminate = ['bye','buy','shutdown','exit','quit','gotosleep','goodbye']
 
+
 class QTextMovieLabel(QLabel):
+    
     def __init__(self, fileName):
         QLabel.__init__(self)
-       
+        thread = Thread(self)
         m = QMovie(fileName)
         m.start()
         self.setMovie(m)
+        app.aboutToQuit.connect(thread.stop)
+        thread.start()
 
     def setMovie(self, movie):
         QLabel.setMovie(self, movie)
@@ -38,83 +40,86 @@ class QTextMovieLabel(QLabel):
         self._movieWidth = s.width()
         self._movieHeight = s.height()
 
-def main():
-    import sys
-    app = QApplication(sys.argv)
-    l = QTextMovieLabel('jelvis.gif')
-    l.setWindowTitle("JELVIS")
-    l.show()
-    app.exec_()
+class Thread(QtCore.QThread):
+    def __init__(self, parent):
+        QtCore.QThread.__init__(self, parent)
+        self.window = parent
+        self._lock = threading.Lock()
+        self.running = True
 
-def internet_on():
-    try:
-        urllib2.urlopen('http://216.58.192.142', timeout=1)
-        return True
-    except urllib2.URLError as err: 
-        return False
+    def run(self):
+        self.running = True
+        
+        def internet_on():
+            try:
+                urllib2.urlopen('http://216.58.192.142', timeout=1)
+                return True
+            except urllib2.URLError as err: 
+                return False
 
-def offline_speak(jarvis_speech):
-    engine = pyttsx3.init()
-    engine.say(jarvis_speech)
-    engine.runAndWait()
 
-def listen():
-    #try:
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("Talk to JELVIS: ")
-        audio = r.listen(source)
-    try:
-        #print (r.recognize_google(audio))
-        #return  r.recognize_google(audio)
-        if internet_on() == True:
-            print (r.recognize_google(audio))
-            return  r.recognize_google(audio)
+        def speak(jarvis_speech):
+           tts = gTTS(text=jarvis_speech, lang='en')
+           tts.save('jarvis_speech.mp3')
+           mixer.init()
+           mixer.music.load('jarvis_speech.mp3')
+           mixer.music.play()
+           while mixer.music.get_busy():
+               time.sleep(1)
+
+        def offline_speak(jarvis_speech):
+            engine = pyttsx3.init()
+            engine.say(jarvis_speech)
+            engine.runAndWait()
+
+        def listen():
+            r = sr.Recognizer()
+            with sr.Microphone() as source:
+                print("Talk to JELVIS: ")
+                audio = r.listen(source)
+            try:
+                if internet_on() == True:
+                   print r.recognize_google(audio)
+                   return  r.recognize_google(audio)
+                else:
+                   print r.recognize_sphinx(audio)
+                   return  r.recognize_sphinx(audio)
+
+            except sr.UnknownValueError:
+                return(listen())
+            except sr.RequestError as e:
+                print("Could not request results from speech service; {0}".format(e))
+
+
+        kernel = aiml.Kernel()
+        if os.path.isfile("bot_brain.brn"):
+            kernel.bootstrap(brainFile = "bot_brain.brn")
         else:
-            print (r.recognize_sphinx(audio))
-            return  r.recognize_sphinx(audio)
-
-    except sr.UnknownValueError:
-        #offline_speak("I couldn't understand what you said! Would you like to repeat?")
-        return(listen())
-    except sr.RequestError as e:
-        print("Could not request results from sphinx service; {0}".format(e))
-    #except sr.UnknownValueError:
-    #    return(listen())
-
-
-kernel = aiml.Kernel()
-
-if os.path.isfile("bot_brain.brn"):
-    kernel.bootstrap(brainFile = "bot_brain.brn")
-else:
-    kernel.bootstrap(learnFiles = "std-startup.xml", commands = "load aiml b")
-    #kernel.saveBrain("bot_brain.brn")
-
-# kernel now ready for use
-def orgin():
-    try:
+            kernel.bootstrap(learnFiles = "std-startup.xml", commands = "load aiml b")
+            
         while True:
             if mode == "voice":
                 response = listen()
             else:
                 response = raw_input("Talk to JELVIS : ")
             if response.lower().replace(" ","") in terminate:
-                #break
                 response = listen()    
             jarvis_speech = kernel.respond(response)
             print ("JELVIS: " + jarvis_speech)
             offline_speak(jarvis_speech)
-    except:
-       return(orgin())
 
-
-t = threading.Thread(name='orgin', target=orgin)
-w = threading.Thread(name='main', target=main)
-
-w.start()
-t.start()
+    def stop(self):
+        engine = pyttsx3.init()
+        engine.say("goodbye sir")
+        engine.runAndWait()
+        self.running = False
 
 
 
- 
+if __name__ == '__main__':
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    l = QTextMovieLabel('jelvis.gif')
+    l.setWindowTitle("JELVIS")
+    l.show()
+    sys.exit(app.exec_())
