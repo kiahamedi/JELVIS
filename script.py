e129d8aa2b29524ee0757342177e9d30e988d7aa
kia hamedi <kia.arta9793@gmail.com>
2019-04-29 00:03:00 +0430

    fix some bugs

--- script.py
+++ script.py
@@ -1,6 +1,8 @@
 import aiml
 import os
 import time, sys
+#from gtts import gTTS
+#from pygame import mixer
 import pyttsx3
 import warnings
 import threading
@@ -11,6 +13,7 @@ import speech_recognition as sr
 from PyQt4.QtCore import QSize
 from PyQt4.QtGui import QApplication, QLabel, QMovie, QPainter, QFontMetrics 
 import urllib3
+
 
 mode = "voice"
 if len(sys.argv) > 1:
@@ -24,7 +27,7 @@ terminate = ['bye','buy','shutdown','exit','quit','gotosleep','goodbye']
 
 
 class QTextMovieLabel(QLabel):
-    
+
     def __init__(self, fileName):
         QLabel.__init__(self)
         thread = Thread(self)
@@ -40,6 +43,8 @@ class QTextMovieLabel(QLabel):
         self._movieWidth = s.width()
         self._movieHeight = s.height()
 
+
+ 
 class Thread(QtCore.QThread):
     def __init__(self, parent):
         QtCore.QThread.__init__(self, parent)
@@ -49,7 +54,7 @@ class Thread(QtCore.QThread):
 
     def run(self):
         self.running = True
-        
+
         def internet_on():
             try:
                 urllib2.urlopen('http://216.58.192.142', timeout=1)
@@ -78,35 +83,46 @@ class Thread(QtCore.QThread):
                 print("Talk to JELVIS: ")
                 audio = r.listen(source)
             try:
-                if internet_on() == True:
-                   print r.recognize_google(audio)
-                   return  r.recognize_google(audio)
-                else:
-                   print r.recognize_sphinx(audio)
-                   return  r.recognize_sphinx(audio)
+                print (r.recognize_google(audio))
+                return  r.recognize_google(audio)
+                #if internet_on() == True:
+                #    print r.recognize_google(audio)
+                #    return  r.recognize_google(audio)
+                #else:
+                #    print r.recognize_sphinx(audio)
+                #    return  r.recognize_sphinx(audio)
 
             except sr.UnknownValueError:
+                #offline_speak("I couldn't understand what you said! Would you like to repeat?")
                 return(listen())
             except sr.RequestError as e:
                 print("Could not request results from speech service; {0}".format(e))
-
+            #except sr.UnknownValueError:
+            #    return(listen())
 
         kernel = aiml.Kernel()
+
         if os.path.isfile("bot_brain.brn"):
             kernel.bootstrap(brainFile = "bot_brain.brn")
         else:
             kernel.bootstrap(learnFiles = "std-startup.xml", commands = "load aiml b")
-            
+            #kernel.saveBrain("bot_brain.brn")
+        # kernel now ready for use
         while True:
             if mode == "voice":
                 response = listen()
             else:
                 response = raw_input("Talk to JELVIS : ")
             if response.lower().replace(" ","") in terminate:
+                #break
                 response = listen()    
             jarvis_speech = kernel.respond(response)
             print ("JELVIS: " + jarvis_speech)
             offline_speak(jarvis_speech)
+            #if internet_on() == True:
+            #    speak(jarvis_speech)
+            #else:
+            #    offline_speak(jarvis_speech)
 
     def stop(self):
         engine = pyttsx3.init()
